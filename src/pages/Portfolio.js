c5421675539c88a413e50c33e1620e5cfd41098b
Tyrano-jpg <tanmay.kdc@gmail.com>
2025-02-21 13:20:54 +0530

    Update Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -39,10 +39,26 @@ const PortfolioSection = () => {
     <section>
       <Box
         sx={{
-          px: { xs: 2, md: 10, lg: 1, xl: 5 },
+          px: { xs: 0, md: 10, lg: 1, xl: 5 },
           py: 4,
-          mx: "55px",
+          mx: { xs: "40px", md: "55px" },
+          "@media(min-width: 768px)": {
+            mx: "70px",
+          },
+          "@media(min-width: 1024px)": {
+            width: "900px",
+            padding: "10px 0",
+          },
+          "@media(min-width: 1240px)": {
+            width: "1100px",
+            padding: "10px 0",
+            margin: "10px auto",
+          },
+          "@media(min-width: 1500px)": {
+            width: "1300px",
+          },
           overflow: "hidden",
+          border: "1px solid red",
         }}
       >
         {/* Typography Section */}
@@ -50,15 +66,42 @@ const PortfolioSection = () => {
           sx={{
             paddingLeft: { lg: 6, xl: 6 },
             maxWidth: { md: "1000px", lg: "1000px", xl: "1300px" },
+            border: "1px solid blue",
+            "@media(min-width: 1024px)": {
+              margin: "10px 0px",
+            },
+            "@media(min-width: 1240px)": {
+              margin: "10px -25px",
+            },
+            "@media(min-width: 1500px)": {
+              paddingLeft: "30px",
+            },
           }}
           className="wow animate__animated animate__fadeInDown"
           data-wow-duration="1.5s"
         >
           <Box sx={{ textAlign: "left", maxWidth: "800px" }}>
-            <Typography variant="h5" sx={{ fontWeight: "bold", mt: 2, mb: 2 }}>
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: "bold",
+                mt: 2,
+                mb: 2,
+                fontSize: { xs: "17px", md: "22px" },
+                "@media(min-width: 1500px)": {
+                  fontSize: "25px",
+                },
+              }}
+            >
               “Digital Solutions that Deliver Results”
             </Typography>
-            <Typography variant="body1" sx={{ mb: 4 }}>
+            <Typography
+              variant="body1"
+              sx={{ mb: 4, fontSize: { xs: "13px", md: "15px", fontSize: { xs: "17px", md: "22px" },
+              "@media(min-width: 1500px)": {
+                fontSize: "18px",
+              }, } }}
+            >
               Discover our diverse portfolio showcasing innovative solutions and
               applications that drive success. Each project reflects our
               commitment to quality, creativity, and user-centered design.
@@ -89,6 +132,9 @@ const PortfolioSection = () => {
                     selectedFilter === filter ? "#C8102E" : "transparent",
                   border: "1.3px solid black",
                   px: 2,
+                  height: { xs: "30px", md: "40px", lg: "50px" },
+                  width: { xs: "100px", md: "120px", lg: "140px" },
+                  fontSize: { xs: "10px", md: "12px", lg: "15px" },
                   "&:hover": {
                     backgroundColor: "#C8102E",
                     color: "#fff",
@@ -102,7 +148,7 @@ const PortfolioSection = () => {
         </Box>
 
         {/* Image Grid - 2 Per Row */}
-        <Box
+        {/* <Box
           sx={{
             display: "grid",
             gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr" },
@@ -110,11 +156,52 @@ const PortfolioSection = () => {
             justifyContent: "center",
             maxWidth: { md: "80%", lg: "1000px", xl: "1450px" },
             "@media(min-width: 1728px)": {
-              width: "1300px",
+              width: "1528px",
+              padding: "20px 10px",
+              marginRight: "-20px",
+              marginLeft: "85px",
             },
+            "@media(min-width: 1680px)": {
+                  width: "1300px",
+                },
             mx: "auto",
             py: 3,
             px: { md: 10, lg: 10, xl: -10 },
+          }}
+        > */}
+        <Box
+          sx={{
+            border: "1px solid green",
+            display: "grid",
+            gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr" },
+            gap: "40px",
+            "@media(min-width: 768px)": {
+              gap: "20px",
+            },
+            "@media(min-width: 320px)": {
+              gap: "15px",
+            },
+            "@media(min-width: 425px)": {
+              padding: "10px 0px",
+            },
+            "@media(min-width: 1024px)": {
+              width: "1300px",
+            },
+            "@media(min-width: 1500px)": {
+              margin: "10px auto",
+            },
+            justifyContent: "center",
+            maxWidth: { md: "90%", lg: "85%", xl: "80%" },
+            margin: "0 auto",
+            py: 3,
+            px: { xs: 2, md: 4, lg: 5, xl: 8 },
+            "@media(min-width: 1728px)": {
+              maxWidth: "1648px",
+              margin: "0 20px",
+            },
+            "@media(min-width: 1680px)": {
+              maxWidth: "1320px", // Increased width for large screens
+            },
           }}
         >
           {filteredItems.map((item) => (
@@ -128,11 +215,43 @@ const PortfolioSection = () => {
                   lg: "500px",
                   xl: "700px",
                 },
+                "@media(min-width: 320px)": {
+                  width: "240px",
+                  height: "170px",
+                },
+                "@media(min-width: 375px)": {
+                  width: "270px",
+                  height: "170px",
+                },
+                "@media(min-width: 425px)": {
+                  width: "345px",
+                  height: "190px",
+                },
+                "@media(min-width: 768px)": {
+                  width: "304px",
+                  height: "190px",
+                },
+                "@media(min-width: 1024px)": {
+                  width: "430px",
+                  height: "250px",
+                },
+                "@media(min-width: 1240px)": {
+                  width: "515px",
+                  height: "290px",
+                },
                 "@media(min-width: 1500px)": {
-                  width: "595px",
+                  width: "640px",
+                  height: "350px",
                 },
                 "@media(min-width: 1728px)": {
-                  width: "700px",
+                  width: "714px",
+                  height: "438",
+                  margin: "0",
+                  paddingRight: "-10px",
+                },
+                "@media(min-width: 1680px)": {
+                  width: "674px",
+                  height: "418",
                 },
                 height: { md: "300px", lg: "330px", xl: "400px" }, // Increased height by 50px
                 borderRadius: 2,
