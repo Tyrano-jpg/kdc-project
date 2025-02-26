e398c119b6ea90fca6c945e8c28a4d62753df687
Tyrano-jpg <tanmay.kdc@gmail.com>
2025-02-26 17:11:59 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,85 +1,125 @@
-import './Footer.css';
-import logo from '../images/KDC_logo.png';
-import facebook from '../images/Facebook.png';
-import instagram from '../images/Instagram.png';
-import linkedin from '../images/Linkedin.png';
-import { useInView } from 'react-intersection-observer';
-import { motion } from 'framer-motion';
+import { Box, Typography, Stack, Link } from "@mui/material";
+import kdclogo from "../images/KDC_logo.png";
+import facebookimg from "../images/Facebook.png";
+import instagramimg from "../images/Instagram.png";
+import linkedinimg from "../images/Linkedin.png";
 
-function Footer() {
-    const fadeInVariants = {
-        hidden: { opacity: 0, y: 50 },
-        visible: { opacity: 1, y: 0, transition: { duration: 0.6 } }
-    };
+const Footer = () => {
+  return (
+    <footer>
+      <Box
+        sx={{
+          backgroundColor: "#fff",
+          py: 4,
+          px: { xs: 3, sm: 5, md: 10 },
+          borderTop: "1px solid black",
+          textAlign: { xs: "center", md: "left" },
+        }}
+      >
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          justifyContent="space-between"
+          alignItems={{ xs: "center", md: "flex-start" }}
+          flexWrap="wrap"
+          gap={{ xs: 4, md: 3 }}
+        >
+          {/* Left Column - Logo & Description */}
+          <Box sx={{ maxWidth: { xs: "100%", md: "350px" } }}>
+            <img src={kdclogo} alt="KDigitalCurry" width="170px" height="auto" />
+            <Typography variant="body2" color="text.secondary" mt={1} sx={{ lineHeight: 1.6 }}>
+              KDigitalCurry is a leading mobile and web app development company in Mumbai with over 10+ years of
+              experience. We create human-centric, scalable solutions, delivering full-stack services across various
+              domains.
+            </Typography>
 
-    const [refOne, inViewOne] = useInView({ triggerOnce: true, threshold: 0.2 });
-    const [refTwo, inViewTwo] = useInView({ triggerOnce: true, threshold: 0.2 });
-    const [refThree, inViewThree] = useInView({ triggerOnce: true, threshold: 0.2 });
+            {/* Social Icons */}
+            <Stack direction="row" spacing={2} mt={2} justifyContent={{ xs: "center", md: "flex-start" }}>
+              {[
+                { src: facebookimg, alt: "Facebook", link: "https://www.facebook.com" },
+                { src: instagramimg, alt: "Instagram", link: "https://www.instagram.com" },
+                { src: linkedinimg, alt: "LinkedIn", link: "https://www.linkedin.com" },
+              ].map((icon, index) => (
+                <Link key={index} href={icon.link} target="_blank">
+                  <img src={icon.src} alt={icon.alt} style={{ height: "35px", width: "35px" }} />
+                </Link>
+              ))}
+            </Stack>
+          </Box>
 
-    return (
-        <div className='footer'>
-            <footer>
-                <motion.div
-                    ref={refOne}
-                    initial="hidden"
-                    animate={inViewOne ? "visible" : "hidden"}
-                    variants={fadeInVariants}
-                    className='col-one'
+          {/* Middle Column - Services */}
+          <Box sx={{ textAlign: { xs: "center", md: "left" } }}>
+            <Typography fontWeight="bold" sx={{ mb: 1 }}>
+              Services
+            </Typography>
+            <Box>
+              {[
+                "Mobile App Development",
+                "Web & CMS Development",
+                "ECommerce Development",
+                "Search Engine Optimization",
+                "Hire Dedicated Resources",
+                "UI / UX Design",
+                "Custom Software Development",
+              ].map((service, index) => (
+                <Link
+                  key={index}
+                  href="#"
+                  sx={{
+                    display: "block",
+                    my: 0.8,
+                    textDecoration: "none",
+                    color: "black",
+                    fontSize: "15px",
+                    "&:hover": { textDecoration: "underline" },
+                  }}
                 >
-                    <img src={logo} alt='logo'/>
-                    <p>KDgitialCurry is a Leading Mobile and Web App 
-                    Development Company in Mumbai with over 10+ Years of Experience. 
-                    We create human-centric, scalable solutions delivering full-stack services 
-                    across various domains.</p>
+                  • {service}
+                </Link>
+              ))}
+            </Box>
+          </Box>
 
-                    <div className='socio-icon'>
-                        <img src={facebook} alt='facebook'/>
-                        <img src={instagram} alt='instagram' />
-                        <img src={linkedin} alt='linkedin'/>
-                    </div>
-                </motion.div>
-
-                <motion.div
-                    ref={refTwo}
-                    initial="hidden"
-                    animate={inViewTwo ? "visible" : "hidden"}
-                    variants={fadeInVariants}
-                    className='col-two'
+          {/* Right Column - Careers */}
+          <Box sx={{ textAlign: { xs: "center", md: "left" } }}>
+            <Typography fontWeight="bold" sx={{ mb: 1 }}>
+              Explore Careers
+            </Typography>
+            <Box>
+              {[
+                "Sr. PHP Developer",
+                "Sr. MERN Developer",
+                "IOS Developer",
+                "UI / UX Designer",
+                "Flutter Developer",
+                "Project Manager",
+                "Android Developer",
+              ].map((career, index) => (
+                <Link
+                  key={index}
+                  href="#"
+                  sx={{
+                    display: "block",
+                    my: 0.8,
+                    textDecoration: "none",
+                    color: "black",
+                    fontSize: "15px",
+                    "&:hover": { textDecoration: "underline" },
+                  }}
                 >
-                    <p>Services</p>
-                    <ul>
-                        <li>Mobile App Development</li>
-                        <li>Web & CMS Development</li>
-                        <li>ECommerce Development</li>
-                        <li>Search Engine Optimization</li>
-                        <li>Hire Dedicated Resource</li>
-                        <li>UI / UX Design</li>
-                        <li>Custom Software Development</li>
-                    </ul>
-                </motion.div>
+                  • {career}
+                </Link>
+              ))}
+            </Box>
+          </Box>
+        </Stack>
 
-                <motion.div
-                    ref={refThree}
-                    initial="hidden"
-                    animate={inViewThree ? "visible" : "hidden"}
-                    variants={fadeInVariants}
-                    className='col-three'
-                >
-                    <p>Explore Careers</p>
-                    <ul>
-                        <li>Sr. PHP Developer</li>
-                        <li>Sr. MERN Developer</li>
-                        <li>IOS Developer</li>
-                        <li>UI / UX Designer</li>
-                        <li>Flutter Developer</li>
-                        <li>Project Manager</li>
-                        <li>Android Developer</li>
-                    </ul>
-                </motion.div>
-            </footer>
-            <p className='copyright'>Copyright @ 2024 KDIGITALCURRY. All Rights Reserved</p>
-        </div>
-    );
-}
+        {/* Bottom Copyright */}
+        <Typography variant="body2" textAlign="center" mt={7} color="text.secondary">
+          Copyright © 2024 K DIGITALCURRY. All Rights Reserved
+        </Typography>
+      </Box>
+    </footer>
+  );
+};
 
 export default Footer;
